18776e2d93eea1b68444cf384c9c4d98d3a4e1cc
André Santana Nunes <38633722+andresnunes@users.noreply.github.com>
2026-02-19 21:38:31 -0300

    testes novos

--- s4m3-futuro-v45/src/test/java/com/example/s4m3futurov45/S4m3FuturoV45ApplicationTests.java
+++ s4m3-futuro-v45/src/test/java/com/example/s4m3futurov45/S4m3FuturoV45ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.s4m3futurov45;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest // Contexto do Spring
 class S4m3FuturoV45ApplicationTests {
 
     @Test
