737d8a3d305b659b084bfbccc41614a2b8d03428
André Santana Nunes <38633722+andresnunes@users.noreply.github.com>
2026-02-19 21:28:03 -0300

    teste com falha adicionado

---
+++ s4m3-futuro-v45/src/test/java/com/example/s4m3futurov45/TesteFalha.java
@@ -0,0 +1,14 @@
+package com.example.s4m3futurov45;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TesteFalha {
+
+    @Test // Método de teste
+    public void testFalha(){
+        assertTrue(false); // validação se algo é True
+    }
+
+}
